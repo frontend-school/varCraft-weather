dce61d25f4c5164a898a36142e184732c53229f5
Eldar Khaitov <benzin.a95@list.ru>
2015-04-03 23:49:34 +0300

    New files added: package.json and bower.json. Gulpfile.js edited. Project is ready for promotion.

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,46 @@
-//required variables
 var gulp = require('gulp'),
-    livereload = require('gulp-livereload'),
-    del = require('del');
+    del = require('del'),
+    webserver = require('gulp-webserver');
 
-//environment variables
-//I discovered that gulp tasks cannot receive input data so I use globals
 var source = "app/**",
     destination = "dist";
 
-// Default Task
 gulp.task('default', ['watch']);
 
-//-- file handler ------------------------------------------------------------------------------------------------------
-//Copy from source to destination (app-dist default)
-gulp.task('kopi', function () {
+gulp.task('copy', function () {
     gulp.src(source)
         .pipe(gulp.dest(destination));
 
-    //reset globals
     source = "app/**";
     destination = "dist";
 });
 
-gulp.task('klin', function() {
+gulp.task('clean', function() {
     del(destination);
+    destination = "dist";
 });
-//----------------------------------------------------------------------------------------------------------------------
 
-//Copy js scripts to vendor folder
 gulp.task('bc-vendor', function(){
     source = "bower_components/**/*.js";
     destination = "dist/vendor";
-    gulp.start(['kopi']);
+    gulp.start(['copy']);
 });
 
-//Watch Task
-//If standard gulp copy function copies entire folder structure regardless of how many files were changed
-//  then this task makes it better;
-//else
-//  I have written a lot of extra code :(
 gulp.task('watch', function(){
     gulp.watch('app/**', function(event) {
-        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
         source = event.path.toString();
         destination = source.replace("\\app\\", "\\dist\\");
         if(event.type == 'deleted')
         {
-            gulp.start(['klin']);
+            gulp.start(['clean']);
         }
-        //if type is: added, changed or renamed. Works well, tested
         else
         {
             destination = destination.slice(0, destination.lastIndexOf('\\')); //remove filename with extension. Only for creating files needed
-            gulp.start(['kopi']);
+            gulp.start(['copy']);
         }
     });
 });
-
-//Livereload Task
-var webserver = require('gulp-webserver');
 
 gulp.task('webserver', function() {
     gulp.src('app')

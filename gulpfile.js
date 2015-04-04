834ed054abb29bcdecc51698690e9e00957d2dc6
Stas-Dolgachov <s.dolgachov@gmail.com>
2015-04-04 19:45:48 +0300

    gulpfile modified, removed all extra gulp plugins, gulp-sass plugin added

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,81 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var bowerSrc = require('gulp-bower-src');
+var sass = require( 'gulp-sass' );
+
+var source = "app";
+var destination = "dist";
+var filteredSource = [source + '/css/**/*.css',
+                      source + '/css/**/*.scss',
+                      source + '/js/**/*.js',
+                      source + '/img/**/*.*',
+                      source + '/index.html'];
+
+var filteredDestination = ['dist/css/*.css',
+                            'dist/js/*.js',
+                            'dist/*.html',
+                            'dist/img/*.*'];
+
+var appComponents  = "bower_components";
+
 
 gulp.task('server', function() {
     browserSync({
+            port: 8080,
             server: {
-            baseDir: "dist"
+            baseDir: destination
         }
     });
 });
 
 
 gulp.task('html', function() {
-    gulp.src('app/*.html')
-    .pipe(gulp.dest('dist'));
+    gulp.src(source + '/*.html')
+    .pipe(gulp.dest(destination));
 })
 
 gulp.task('js', function() {
-    gulp.src('app/js/**')
-    .pipe(gulp.dest('dist/js'))
+    gulp.src(source + '/js/**')
+    .pipe(gulp.dest(destination + '/js'))
 })
 
-gulp.task('css', function() {
-    gulp.src('app/css/**')
-    .pipe(gulp.dest('dist/css'))
+gulp.task( 'css', function() {
+
+    gulp.src(source + '/css/*.css')
+        .pipe(gulp.dest(destination + '/css'));  //copy common CSS files, usefull during aprobation period with SASS
+
+    gulp.src(source + '/css/*.scss')
+    .pipe(sass())
+    .pipe( gulp.dest( destination + '/css' ) ); //transform files from SCSS to CSS and copy them to "dist/css"
 })
 
 gulp.task('img', function() {
-    gulp.src('app/img/**')
-    .pipe(gulp.dest('dist/img'))
+    gulp.src(source + '/img/**')
+    .pipe(gulp.dest(destination + '/img'))
 })
 
-gulp.task('build', ['html', 'css', 'js', 'img']);
-
-
-;
 
 gulp.task('bower', function () {
     bowerSrc()
-        .pipe(gulp.dest('dist/vendor'));
+        .pipe(gulp.dest(destination + '/vendor'));
 });
 
 //watcher
+gulp.task('watch-source', function() {
+    gulp.watch(filteredSource, ['build']);
+})
 
-gulp.task( 'watch', function() {
-
-    gulp.watch('app/*.html', ['html'] );
-    gulp.watch('app/js/**', ['js']);
-    gulp.watch('app/css/**', ['css']);
-    gulp.watch('app/img/**', ['img']);
-    gulp.watch('bower_components/**', ['bower']);
-
-    gulp.watch(['dist/css/*.css', 'dist/js/*.js', 'dist/*.html', 'dist/img/*.*']).on('change', browserSync.reload);
-    
-
-});
+gulp.task('watch-components', function() {
+    gulp.watch(appComponents, ['bower']);
+})
 
 
-gulp.task('build', ['html', 'css', 'js', 'img', 'bower']);
+gulp.task('livereload', function() {
+            gulp.watch(filteredDestination).on('change', browserSync.reload);
+})
 
-gulp.task("default", ['server', 'build', 'watch']);
+gulp.task('watcher', ['watch-source', 'watch-components']);
 
+gulp.task('build', ['html', 'css', 'js', 'img']);
+
+gulp.task("default", ['server', 'build', 'watcher', 'livereload']);

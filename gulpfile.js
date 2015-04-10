e735fe5405ee41b41323400f39d11fc237cb4119
Eldar Khaitov <benzin.a95@list.ru>
2015-04-10 22:16:53 +0300

    mes

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     del = require('del'),
-    webserver = require('gulp-webserver');
+    webserver = require('gulp-webserver'),
+    sass = require('gulp-sass');
 
 var source = "app/**",
     destination = "dist";
@@ -26,20 +27,39 @@ gulp.task('bc-vendor', function(){
     gulp.start(['copy']);
 });
 
+gulp.task('sass',function(){
+    gulp.src(source)
+        .pipe(sass({
+            outputStyle: 'nested', //compressed for uglification
+            sourceComments: 'map',
+            includePaths: [source]
+        }))
+        .pipe(gulp.dest(destination));
+});
+
 gulp.task('watch', function(){
-    gulp.watch('app/**', function(event) {
-        source = event.path.toString();
-        destination = source.replace("\\app\\", "\\dist\\");
-        if(event.type == 'deleted')
-        {
-            gulp.start(['clean']);
-        }
-        else
-        {
-            destination = destination.slice(0, destination.lastIndexOf('\\')); //remove filename with extension. Only for creating files needed
-            gulp.start(['copy']);
-        }
-    });
+    try {
+        gulp.watch('app/**', function (event) {
+            source = event.path.toString();
+            destination = source.replace("\\app\\", "\\dist\\");
+            if (event.type == 'deleted') {
+                gulp.start(['clean']);
+            }
+            else {
+                destination = destination.slice(0, destination.lastIndexOf('\\'));
+                var fileExtension = source.slice(source.lastIndexOf('.'));
+                if (fileExtension == '.scss') {
+                    gulp.start(['sass']);
+                }
+                else {
+                    gulp.start(['copy']);
+                }
+            }
+        });
+    }
+    catch(error){
+        gulp.start(['watch']);
+    }
 });
 
 gulp.task('webserver', function() {

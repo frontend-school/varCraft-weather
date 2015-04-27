6d201136530b44ee24fbbe416c52d24111a426e8
abramova_alex <abramova.alex@gmail.com>
2015-04-27 10:46:06 +0300

    Remove b- prefix

--- app/js/dashboardModule.js
+++ app/js/dashboardModule.js
@@ -2,13 +2,13 @@ MYAPPLICATION.dashboardModule = (function () {
     var greeting = "Hello ";
     return {
         hideDashboard: function () {
-            MYAPPLICATION.helperModule.getElement(MYAPPLICATION.CONST.ID.dashboardGreating).innerHTML = "";
+            MYAPPLICATION.helperModule.getElement(MYAPPLICATION.CONST.ID.dashboardGreeting).innerHTML = "";
             MYAPPLICATION.helperModule.getElement(MYAPPLICATION.CONST.ID.dashboard).className = MYAPPLICATION.helperModule.getElement(MYAPPLICATION.CONST.ID.dashboard).className.replace
             (/(?:^|\s)b-dashboard_active(?!\S)/g, '');
             MYAPPLICATION.helperModule.getElement(MYAPPLICATION.CONST.ID.dashboard).className += MYAPPLICATION.CONST.ID.dashboardHidden;
         },
         showDashboard: function (name) {
-            MYAPPLICATION.helperModule.getElement(MYAPPLICATION.CONST.ID.dashboardGreating).innerHTML = greeting + name;
+            MYAPPLICATION.helperModule.getElement(MYAPPLICATION.CONST.ID.dashboardGreeting).innerHTML = greeting + name;
 
             MYAPPLICATION.helperModule.getElement(MYAPPLICATION.CONST.ID.dashboard).className = MYAPPLICATION.helperModule.getElement(MYAPPLICATION.CONST.ID.dashboard).className.replace
             (/(?:^|\s)b-dashboard_hidden(?!\S)/g, '');

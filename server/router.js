cd92406367f24eac3e934ca2742e87c836c28db3
Triplecorpse <benzin.a95@gmail.com>
2015-05-05 12:48:46 +0300

    Weather app bugs fixed. It runs as should

--- server/router.js
+++ server/router.js
@@ -4,50 +4,15 @@ module.exports = function(app, needReload){
         bodyParser = require('body-parser'),
         pageConstructor = require('./pageConstructor'),
         databaseHandler = require('./databaseHandler'),
-        //passport = require('passport'),
-        //LocalStrategy = require('passport-local').Strategy,
         cookieParser = require('cookie-parser'),
-        http = require('http'),
         authentication = require('./authentication');
 
-    var //login,
-        //password,
-        //refreshedTime,
-        appFolder = '../dist/';
-
-    //app.configure(function() {
-    //    app.use(express.static('public'));
-    //    app.use(express.cookieParser());
-    //    app.use(express.bodyParser());
-    //    app.use(express.session({ secret: 'keyboard cat' }));
-    //    app.use(passport.initialize());
-    //    app.use(passport.session());
-    //    app.use(app.router);
-    //});
-
-    //passport.use(new LocalStrategy(
-    //    function(username, password, done) {
-    //        User.findOne({ username: username }, function(err, user) {
-    //            if (err) { return done(err); }
-    //            if (!user) {
-    //                return done(null, false, { message: 'Incorrect username.' });
-    //            }
-    //            if (!user.validPassword(password)) {
-    //                return done(null, false, { message: 'Incorrect password.' });
-    //            }
-    //            return done(null, user);
-    //        });
-    //    }
-    //));
+    var appFolder = 'dist/';
 
     app.use('/dist', express.static('dist'));
     app.use('/block', express.static('block'));
     app.use('/icon', express.static('icon'));
     app.use('/font', express.static('font'));
-    //app.use(passport.initialize());
-    //app.use(passport.session());
-    //app.use(express.cookieDecoder());
-    //app.use(express.session());
     app.use(bodyParser.urlencoded({extended: false}));
     app.use(cookieParser());
 
@@ -58,16 +23,6 @@ module.exports = function(app, needReload){
         if((status[0] === 0) || (status[0] === 2)) {
             res.redirect('/weather');
         }
-        //res.redirect('/login?login=' + login + '&password=' + password);
-        //if(databaseHandler.userExists(login,password))
-        //{
-        //    res.status(200).send('success');
-        //    res.redirect('/weather');
-        //}
-        //else
-        //{
-        //    res.status(403).send('failed');
-        //}
     });
 
     function generateResponse(res, login, password){
@@ -85,7 +40,6 @@ module.exports = function(app, needReload){
         if((cookieSid) && (!sessionUser)){
             res.clearCookie('sid');
             res.redirect('/');
-            //res.send('cookie cleared');
         }
     }
 
@@ -125,7 +79,6 @@ module.exports = function(app, needReload){
             res.clearCookie('login');
             res.status(403).send({status:"fail", desc:"No active session", sid:null, login:null});
         }
-        //res.redirect('/');
     });
 
     app.get('/ask', function(req, res){
@@ -153,19 +106,6 @@ module.exports = function(app, needReload){
         catch(err){
 
         }
-
-        //var result = {
-        //    'login':login,
-        //    'needReload':needReload,
-        //    'needLogout':(new Date().getTime() - refreshedTime) >= 1800000
-        //};
-        //if(result.needLogout){
-        //    login = '';
-        //    password = '';
-        //}
-        //res.send(result);
-        //needReload = false;
-        //refreshedTime = new Date().getTime();
     });
 
     app.get('/', function (req, res) {
@@ -184,47 +124,26 @@ module.exports = function(app, needReload){
             }
         }
         catch(err){
+            console.log('Error occurred in "/" router');
         }
-        //if(req.cookies.sid > -1) {
-        //    var content = pageConstructor.constructPage({
-        //        'index': appFolder + 'index.html',
-        //        '@content': appFolder + 'block/login-form/login-form.html',
-        //        '@placeHolder1': ''
-        //    });
-        //    res.send(content);
-        //}
-        //else {
-        //    res.redirect('/weather');
-        //}
     });
 
     app.get('/weather', function (req, res) {
         try {
             clear(req, res);
-            //var cookieSid = req.cookies.sid;
-            //var sessionUser = authentication.getUser(cookieSid);
-            //var dt = new Date().getTime();
-            //if((cookieSid) && (!sessionUser)){
-            //    res.clearCookie('sid');
-            //}
 
             var sid = req.cookies.sid;
-            if(sid) {
-                var user = authentication.getUser(sid);
-                var content = pageConstructor.constructPage({
-                    'index': appFolder + 'index.html',
-                    '@content': appFolder + 'block/weather-screen/weather-screen.html',
-                    '@placeHolder1': appFolder + 'block/header-main/header-main.html',
-                    '@user': 'Hello, ' + user.login
-                });
-                res.send(content);
-            }
-            else{
-                res.redirect('/');
-            }
+            var user = authentication.getUser(sid);
+            var content = pageConstructor.constructPage({
+                'index': appFolder + 'index.html',
+                '@content': appFolder + 'block/weather-screen/weather-screen.html',
+                '@placeHolder1': appFolder + 'block/header-main/header-main.html',
+                '@user': 'Hello, ' + user.login
+            });
+            res.send(content);
         }
         catch(err){
-            //res.redirect('/');
+            console.log('Error occurred in "/weather" router');
         }
     });
 };

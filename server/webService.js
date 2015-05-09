56eea562528b21cbe7c3d98deca92a1c15cb0f2b
Eldar Khaitov <benzin.a95@list.ru>
2015-05-09 12:37:13 +0300

    Memory leak detected and fixed: you could login many times from the same browser and session array was never emptied

--- server/webService.js
+++ server/webService.js
@@ -9,15 +9,19 @@ module.exports = function(app, needReload) {
         var login = req.query.login;
         var password = req.query.password;
         var dt = new Date().getTime();
+        var sid = req.cookies.sid;
+        if((sid) && (!authentication.getUser(login))) {
+            res.status(403).send({status: "fail", desc: "Unclosed session was detected from this browser. Perform /logout operation to clear cookies", sid: null, login: null});
+            return;
+        }
         res.cookie('sid', dt, {expires: new Date(dt + 1800000), httpOnly: true});
-        res.cookie('login', login, {expires: new Date(dt + 1800000), httpOnly: true});
         var status = authentication.addUser(login, password, dt);
 
         if (status === 0) {
-            res.status(200).send({status: "success", desc: "Logined", sid: status[1], login: login});
+            res.status(200).send({status: "success", desc: "Logined", sid: dt, login: login});
         }
         else if (status === 2) {
-            res.status(200).send({status: "success", desc: "Session prolonged", sid: status[1], login: login});
+            res.status(200).send({status: "success", desc: "Session prolonged", sid: dt, login: login});
         }
         else if (status === 1) {
             res.status(403).send({status: "fail", desc: "No such user found", sid: null, login: null});
@@ -42,12 +46,10 @@ module.exports = function(app, needReload) {
         if (sid != null) {
             authentication.removeUser(sid);
             res.clearCookie('sid');
-            res.clearCookie('login');
             res.status(200).send({status: "success", desc: "Logouted", sid: null, login: null});
         }
         else {
             res.clearCookie('sid');
-            res.clearCookie('login');
             res.status(403).send({status: "fail", desc: "No active session", sid: null, login: null});
         }
     });

dee97a26bd580beeaef20fe4ff0c9c6bc698023f
Eldar Khaitov <benzin.a95@list.ru>
2015-05-11 13:23:41 +0300

    Initial commit

--- app/block/header-main/header-main.js
+++
@@ -1,8 +0,0 @@
-function headerMain(){
-    username = document.getElementById('header-main__info_userinfo').innerHTML;
-    username = username.slice(7, username.length - 1);
-    if(username == 'undefined' || username == 'null')
-    {
-        username = undefined;
-    }
-}

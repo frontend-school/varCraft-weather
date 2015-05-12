26429eebab8f8f0f57ab350c4da4df1a0cd46179
a-lysenko <aleksey.lysenko@gmail.com>
2015-05-12 15:55:40 +0300

    Added jshint. Changed structure of code, which descripts pathes to files

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var watch = require('gulp-watch');
 var webserver = require('gulp-webserver');
 var concat = require('gulp-concat');
+var jshint = require('gulp-jshint');
 var sass = require('gulp-ruby-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
@@ -9,91 +10,90 @@ var source = 'app';
 var destination = 'dist';
 var tmpDir = 'tmp';
 
-var filteredSource = [source + '/img/**/*.{png,jpg,jpeg,gif}',
-                      source + '/css/normalize.css',
-                      source + '/index.html'];
-
 var sourceCSS = source + '/css';
-var filteredSourceCSS = sourceCSS + '/**/*.css';
-var filteredSourceSCSS = sourceCSS + '/**/*.scss';
-var destinationCSS = destination + '/css';
-
 var sourceJS = source + '/js';
-var filteredSourceJS = source + '/js/*.js';
-var destinationJS = destination + '/js';
 
-var srcComponents = 'bower_components';
-var destComponents = 'dist/vendor';
+var path = {
+    source: {
+        html: source + '/index.html',
+        img: source + '/img/**/*.{png,jpg,jpeg,gif}',
+        css: [sourceCSS + '/var.scss', sourceCSS + '/style.scss'],
+        cssNormalize: source + '/css/normalize.css',
+        scss: sourceCSS + '/**/*.scss',
+        js: source + '/js/*.js',
+        srcComponents: 'bower_components'
+    },
 
-gulp.task('watch-folder', function() {
-  gulp.src(filteredSource, {base: source})
-    .pipe(watch(filteredSource, {base: source}))
-    .pipe(gulp.dest(destination));
+    destination: {
+        // html: '',
+        // img: '',
+        css: destination + '/css',
+        js: destination + '/js',
+        destComponents: 'dist/vendor'
+    }
+};
+
+gulp.task('watch-folder', function () {
+    var filteredSource = [path.source.html, path.source.img, path.source.cssNormalize];
+
+    gulp.src(filteredSource, {base: source})
+        .pipe(watch(filteredSource, {base: source}))
+        .pipe(gulp.dest(destination));
 });
 
-gulp.task('watch-components-folder', function() {
-  gulp.src(srcComponents, {base: srcComponents})
-    .pipe(watch(srcComponents, {base: srcComponents}))
-    .pipe(gulp.dest(destComponents));
+gulp.task('watch-components-folder', function () {
+    gulp.src(path.source.srcComponents, {base: path.source.srcComponents})
+        .pipe(watch(path.source.srcComponents, {base: path.source.srcComponents}))
+        .pipe(gulp.dest(path.destination.destComponents));
 });
 
 // sass
-gulp.task('sass-concat', function() {
-    return gulp.src([sourceCSS + '/var.scss', sourceCSS + '/style.scss'], {base: sourceCSS})
-    .pipe( concat('style.scss', {newLine: '/*start*/'}) )
-    .pipe(gulp.dest(tmpDir));
+gulp.task('sass-concat', function () {
+    return gulp.src(path.source.css, {base: sourceCSS})
+        .pipe(concat('style.scss', {newLine: '/*start*/'}))
+        .pipe(gulp.dest(tmpDir));
 });
 
-gulp.task('sass', ['sass-concat'], function() {
+gulp.task('sass', ['sass-concat'], function () {
 
     return sass(tmpDir, {style: 'expanded'})
-    .on('error', function (err) {
-        console.error('Error!', err.message);
+        .on('error', function (err) {
+            console.error('Error!', err.message);
         })
-    .pipe(autoprefixer({
-        browsers: ['last 3 versions'],
-        cascade: false
-    }))
-    .pipe(gulp.dest(destinationCSS));
+        .pipe(autoprefixer({
+            browsers: ['last 3 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest(path.destination.css));
 });
 
-// return gulp.src(filteredSourceSCSS, {base: sourceCSS})
-    // .pipe( concat('MY_style.scss', {newLine: '/*start*/'}) )
-    // .pipe( sass(sourceCSS + '/MY_style.scss', {style: 'expanded'})
-    // .on('error', function (err) {
-    //   console.error('Error!', err.message);
-    // }) )
-    // .pipe(autoprefixer({
-    //         browsers: ['last 3 versions'],
-    //         cascade: false
-    //     }))
-    // .pipe(gulp.dest(destinationCSS));
-
-gulp.task('watch-sass', function() {
-  gulp.watch(filteredSourceSCSS, ['sass']);
+gulp.task('watch-sass', function () {
+    gulp.watch(path.source.scss, ['sass']);
 });
 
 // js
-gulp.task('js', function() {
-  return gulp.src( [sourceJS + '/script.js', filteredSourceJS] )
-    .pipe( concat('script.js', {newLine: '/*start*/'}) )
-    .pipe(gulp.dest(destinationJS));
+gulp.task('js', function () {
+    return gulp.src([sourceJS + '/script.js', path.source.js])
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(concat('script.js', {newLine: '/*start*/'}))
+        .pipe(gulp.dest(path.destination.js));
 });
 
-gulp.task('watch-js', function() {
-  gulp.watch(filteredSourceJS, ['js']);
+gulp.task('watch-js', function () {
+    gulp.watch(path.source.js, ['js']);
 });
 
 gulp.task('watch-folders', ['sass', 'js', 'watch-sass', 'watch-js', 'watch-folder', 'watch-components-folder']);
 
-gulp.task('webserver', ['sass', 'js'], function() {
-  gulp.src(destination)
-    .pipe(webserver({
-      port: 8000,
-      livereload: true,
-      open: true
-    }));
+gulp.task('webserver', ['sass', 'js'], function () {
+    gulp.src(destination)
+        .pipe(webserver({
+            port: 8000,
+            livereload: true,
+            open: true
+        }));
 });
 
-gulp.task('default', ['watch-sass','watch-folders', 'webserver']);
+gulp.task('default', ['watch-sass', 'watch-folders', 'webserver']);
 

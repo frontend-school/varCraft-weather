d63a5b38c1cc2e1fcbfb3d9854c4f4efae26e666
abramova_alex <abramova.alex@gmail.com>
2015-05-18 12:20:35 +0300

    implement call to web server[login]

--- app/js/Login/LoginController.js
+++ app/js/Login/LoginController.js
@@ -4,13 +4,21 @@ window.MYAPPLICATION.LoginController = (function () {
     var CONST = window.MYAPPLICATION.CONST;
     return {
         logIn: function () {
-            var user = window.MYAPPLICATION.LoginView.getFormInfo();
+            var user = window.MYAPPLICATION.LoginView.getFormInfo(),
+                loginRequest = new XMLHttpRequest();
+            loginRequest.withCredentials = true;
+
             if (user.login === "" || user.password === "") {
                 return;
             }
-            //call webservise
-            window.MYAPPLICATION.LoginModel.setStatus(user.login);
-            MYAPPLICATION.cookieModule.writeCookie(window.MYAPPLICATION.CONST.cookieName, user.login, window.MYAPPLICATION.CONST.stayTime);
+            loginRequest.onload = function () {
+                if (loginRequest.status === 200) {
+                    window.MYAPPLICATION.LoginModel.setStatus(user.login);
+                    MYAPPLICATION.cookieModule.writeCookie(CONST.cookieName, user.login, CONST.stayTime);
+                }
+            };
+            loginRequest.open('GET', 'http://localhost:3000/login', true);
+            loginRequest.send();
         },
         showDash: function(user) {
             window.MYAPPLICATION.LoginModel.setStatus(user);

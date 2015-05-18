80378d4cbb184867d1e6a5d7d6fe2a1a29fdfafc
abramova_alex <abramova.alex@gmail.com>
2015-05-18 11:54:13 +0300

    implement call to web server

--- app/js/Logout/LogoutController.js
+++ app/js/Logout/LogoutController.js
@@ -5,9 +5,20 @@ window.MYAPPLICATION.LogoutController = (function () {
         cookie = window.MYAPPLICATION.cookieModule;
 
     function _logOut() {
-        var model = window.MYAPPLICATION.LogoutModel;
-        cookie.eraseCookie(CONST.cookieName);
-        model.setStatus(false);
+        var model = window.MYAPPLICATION.LogoutModel,
+            logoutRequest = new XMLHttpRequest();
+        logoutRequest.withCredentials = true;
+
+        logoutRequest.onload = function () {
+            if (logoutRequest.status === 200) {
+                cookie.eraseCookie(CONST.cookieName);
+                model.setStatus(false);
+            }
+        };
+
+        logoutRequest.open('GET', 'http://localhost:3000/logout', true);
+        logoutRequest.send();
+
     }
     return {
         logOut: function () {

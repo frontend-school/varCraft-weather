68349051d3b3b4927736f72d8a73a91e2a0f0341
Eldar Khaitov <benzin.a95@list.ru>
2015-05-19 17:38:58 +0300

    PubSub updated and works well

--- app/block/general.js
+++ app/block/general.js
@@ -1,41 +1,53 @@
 function Topic(subject, text){
-    this.subject = subject;
-    this.text = text;
+    if(typeof subject === 'string') {
+        this.subject = subject;
+    }
+
+    if(typeof text === 'object') {
+        this.text = JSON.stringify(text);
+    }
+    else {
+        this.text = text;
+    }
 }
 
 var topics = [];
 
-function publish(topic){
-    for(var i = topics.length; i >= 0; i--){
-        if((topics[i].subject == topic.subject) && (topics[i].text == topic.text)){
-            console.log('Dublicate!');
-            return {status:'error', desc:'dublicate'};
+function publish(topic, options){
+    options = options || {replace: false};
+    for(var i = topics.length - 1; i >= 0; i--){
+        if((topics[i].subject === topic.subject) && (!options.replace)) {
+            console.log('Duplicate! Use "options.replace = true" to resolve');
+            return {status:'error', desc:'duplicate'};
+        }
+        else if((topics[i].subject === topic.subject) && (options.replace)) {
+            topics[i].text = topic.text;
+            return {status:'success', desc:'added'};
         }
     }
+
     if(topic instanceof Topic) {
         topics.push(topic);
-        return {status:'success', desc:'topic added'};
-    }
-    else{
-        topics.push(new Topic(arguments[0], arguments[1]));
-        return {status:'success', desc:'topic added'};
+        return {status:'success', desc:'added'};
     }
 }
 
-function unpublish(topic){
-    if(topic instanceof Topic) {
-        for(var i = topics.length; i >= 0; i--){
-            if((topics[i].subject == topic.subject) && (topics[i].text == topic.text)){
+function unpublish(subject){
+    if(typeof subject === 'string') {
+        for(var i = topics.length - 1; i >= 0; i--){
+            if(topics[i].subject == subject){
                 topics.splice(i, 1)
             }
         }
     }
 }
 
-function subscribe(topic){
-    for(var i = topics.length; i >= 0; i--){
-        if((topics[i].subject == topic.subject) && (topics[i].text == topic.text)){
-            return topics[i];
+function subscribe(subject){
+    if(typeof subject === 'string') {
+        for (var i = topics.length - 1; i >= 0; i--) {
+            if (topics[i].subject == subject) {
+                return topics[i].text;
+            }
         }
     }
 }

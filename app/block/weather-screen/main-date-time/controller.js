0c3cf41c2ebb087041c2a472ca833e2bd427e018
Triplecorpse <benzin.a95@gmail.com>
2015-05-21 13:07:40 +0300

    Geolocation added

--- app/block/weather-screen/main-date-time/controller.js
+++ app/block/weather-screen/main-date-time/controller.js
@@ -1,6 +1,5 @@
 function mainDateTimeController(){
     publish(new Topic('datetime', new Date().getTime()),{replace:true});
-    //MainDateTimeModel.setDateTime(new Date().getTime());
     mainDateTimeView();
     setTimeout(mainDateTimeController, 1000);
 }
